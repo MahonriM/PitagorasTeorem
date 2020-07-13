fea9d1044060c6fa4c42ce18c8c26886e16ebb34
MahonriM <cripto3000z@gmail.com>
2020-07-13 13:06:16 -0500

    Create pitagora.rb

---
+++ pitagora.rb
@@ -0,0 +1,13 @@
+#Teorema de pitagoras
+def c(a,b)
+    puts Math.sqrt(a**2 + b**2)
+end
+def b(c,a)
+    puts Math.sqrt(c**2-a**2)
+end
+def a(c,b)
+    puts Math.sqrt(c**2-b**2)
+end
+c(4,3)
+b(5,4)
+a(5,3)
